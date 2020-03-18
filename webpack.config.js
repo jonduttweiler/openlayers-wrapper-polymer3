89a6873adb537b43613b0abcfe415260d3a002cb
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2020-03-18 08:09:31 -0300

    webpack-dev-server configured

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ const path = require('path');
 module.exports = {
     entry:'./src/index.js',
     mode: 'development',
+    devtool: 'inline-source-map',
     output: {
         filename:'main.js',
         path: path.resolve(__dirname,'dist')
+    },
+    devServer:{
+        contentBase: './dist'
     }
 }
