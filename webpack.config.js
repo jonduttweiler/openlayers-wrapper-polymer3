c3669fdd3abd0ab4c2721b01440482ca9535aeae
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2020-03-29 11:14:26 -0300

    using terserPlugin to minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry:'./src/index.js',
@@ -8,50 +9,22 @@ module.exports = {
         filename:'main.js',
         path: path.resolve(__dirname,'dist')
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
     devServer:{
         contentBase: './dist'
     },
     module: {
         rules: [
-        /*     {
-                test: /\.(html)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                    }
-                },],
-            }, */
             {
                 test: /\.css$/,
                 use: [
                     'style-loader',
                     'css-loader'
                 ]
-            }, /* {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'img',
-                        publicPath: 'img'
-                    }
-                }
-
-                ],
-            }, */
-         /*    {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts',
-                        publicPath: 'fonts'
-                    }
-                },],
-            }, */
+            },
         ]
     }
 }
